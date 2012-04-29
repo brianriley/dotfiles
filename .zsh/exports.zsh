6dd4fbde668075317c5fa61738a71e2e61c7cecb
Brian Riley <brian@btriley.com>
2012-04-29 10:27:24 -0700

    Added cabal bin dir to path

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -2,4 +2,4 @@ export HISTFILE=$ZSH/data/history
 export HISTSIZE=1000
 export SAVEHIST=$HISTSIZE
 
-export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/share/python:/usr/local/bin:$PATH
+export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.cabal/bin:/usr/local/share/python:/usr/local/bin:$PATH
