d3b608c2bddf75dc182653d0498c72cb05e4d37c
Brian Riley <brian@btriley.com>
2012-05-04 12:00:06 -0700

    Cleaned up the path

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -2,4 +2,15 @@ export HISTFILE=$ZSH/data/history
 export HISTSIZE=1000
 export SAVEHIST=$HISTSIZE
 
-export PATH=$HOME/bin:$HOME/.local/bin:$HOME/.cabal/bin:/usr/local/share/python:/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:/usr/local/bin:${PATH}
+export PATH="/usr/local/share/python:$PATH"
+
+# add Ruby executable path
+RUBY_EXECUTABLE_PATH=`gem environment | grep "EXECUTABLE DIRECTORY" | awk '{print $4}'`
+export PATH="$RUBY_EXECUTABLE_PATH:$PATH"
+
+# add cabal to path
+export PATH="$HOME/.cabal/bin:$PATH"
+
+# add home bin folders to path
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
