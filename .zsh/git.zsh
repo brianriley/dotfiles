08c5338e68248a42dc33748c31772e14ab66f26e
Brian Riley <brian@btriley.com>
2012-12-06 21:03:03 -0800

    Trying new git prompt colors

--- .zsh/git.zsh
+++ .zsh/git.zsh
@@ -1,14 +1,14 @@
 # the current branch
 function git_branch_name() {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-    echo "%{$fg[yellow]%}($(git_status_color)${ref#refs/heads/}%{$fg[yellow]%})%{$terminfo[sgr0]%}"
+    echo "($(git_status_color)${ref#refs/heads/}%{$terminfo[sgr0]%})"
 }
 
 function git_status_color() {
     if [[ -n $(git status -s 2> /dev/null) ]]; then
         echo "%{$fg[red]%}"
     else
-        echo ""
+        echo "%{$fg[green]%}"
     fi
 }
 
