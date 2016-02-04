9b9a0ea7ff8ecc958aa1d04bd880d7e836167bf5
Brian Riley <brian@btriley.com>
2016-02-03 19:44:26 -0800

    Only show scroll bar when scrolling

--- system/osx.sh
+++ system/osx.sh
@@ -72,5 +72,8 @@ echo "Require password immediately after sleep or screen saver begins"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
+echo "Only show scrollbars when scrolling"
+defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
+
 echo "Kill affected applications"
 for app in Safari Finder Dock SystemUIServer; do killall "$app" >/dev/null 2>&1; done
