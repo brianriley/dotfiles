f2b882fa4f2c46db07f5dc78ab05c650d94f014c
Brian Riley <97886+brianriley@users.noreply.github.com>
2020-01-28 10:38:26 -0800

    Some cleanup and a first pass at moving windows between screens

--- phoenix.js
+++ phoenix.js
@@ -1,20 +1,48 @@
-Key.on('c', [ 'ctrl', 'alt', 'cmd' ], function () {
+const MODIFIERS = [ 'ctrl', 'alt', 'cmd' ];
 
-  var screen = Screen.main().flippedVisibleFrame();
-  var window = Window.focused();
+/* Pseudo full screen */
+const maximize = (screen, window) => {
+  const newFrame = {
+    x: window.frame().x,
+    y: window.frame().y,
+    width: screen.width - 85,
+    height: screen.height - 55
+  };
 
-  if (window) {
-    const newFrame = {
-      x: window.frame().x,
-      y: window.frame().y,
-      width: screen.width - 85,
-      height: screen.height - 55
-    };
+  window.setFrame(newFrame);
+};
 
-    window.setFrame(newFrame);
-    window.setTopLeft({
-      x: screen.x + (screen.width / 2) - (window.frame().width / 2),
-      y: screen.y + (screen.height / 2) - (window.frame().height / 2)
-    });
-  }
+const center = (screen, window) => {
+  window.setTopLeft({
+    x: screen.x + (screen.width / 2) - (window.frame().width / 2),
+    y: screen.y + (screen.height / 2) - (window.frame().height / 2)
+  });
+};
+
+/* Center */
+Key.on('c', MODIFIERS, () => {
+  const screen = Screen.main().flippedVisibleFrame();
+  const window = Window.focused();
+
+  if (!window || !screen) return;
+
+  maximize(screen, window);
+  center(screen, window);
+});
+
+/* Reload */
+Key.on( 'r', MODIFIERS, () => Phoenix.reload() );
+
+/* Move window */
+Key.on('left', MODIFIERS, () => {
+  const window = Window.focused();
+  const currentScreen = window.screen();
+  const screens = Screen.all();
+
+  const nextScreen = screens
+    .find(screen => screen.identifier() !== currentScreen.identifier())
+    .flippedVisibleFrame();
+
+  maximize(nextScreen, window);
+  center(nextScreen, window);
 });
