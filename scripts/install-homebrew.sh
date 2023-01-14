2a5add9f5418e4217576ba3e2b5c74460b3c2c24
Brian Riley <97886+brianriley@users.noreply.github.com>
2023-01-14 14:41:48 -0800

    Update homebrew install script

--- scripts/install-homebrew.sh
+++ scripts/install-homebrew.sh
@@ -3,6 +3,6 @@
 set -e
 
 if ! command -v brew >/dev/null; then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  export PATH="/usr/local/bin:$PATH"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
