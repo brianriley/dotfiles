90ef14e838fed77f7a8044d20e760e1b6392cc21
Brian Riley <97886+brianriley@users.noreply.github.com>
2023-04-19 12:30:12 -0700

    More plugin config cleanup

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -178,13 +178,13 @@ return {
             if vim.wo.diff then return ']c' end
             vim.schedule(function() gs.next_hunk() end)
             return '<Ignore>'
-            end, {expr=true})
+          end, {expr=true})
 
           map('n', '[c', function()
             if vim.wo.diff then return '[c' end
             vim.schedule(function() gs.prev_hunk() end)
             return '<Ignore>'
-            end, {expr=true})
+          end, {expr=true})
         end
       })
     end,
@@ -253,16 +253,16 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
-event = { "BufReadPost", "BufNewFile" },
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
-  {
-"nvim-treesitter/nvim-treesitter-textobjects",
-init = function()
-  -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-local enabled = false
-if opts.textobjects then
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        init = function()
+          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+          local enabled = false
+          if opts.textobjects then
             for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
               if opts.textobjects[mod] and opts.textobjects[mod].enable then
                 enabled = true
@@ -280,9 +280,9 @@ if opts.textobjects then
       { "<c-space>", desc = "Increment selection" },
       { "<bs>", desc = "Decrement selection", mode = "x" },
     },
--- @type TSConfig
+    -- @type TSConfig
     opts = {
-  highlight = { enable = true },
+      highlight = { enable = true },
       indent = { enable = true },
       context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = {
@@ -303,30 +303,29 @@ if opts.textobjects then
         "typescript",
         "vim",
         "vimdoc",
-"yaml",
-},
+        "yaml",
+      },
       incremental_selection = {
         enable = true,
         keymaps = {
-  init_selection = "<C-space>",
-node_incremental = "<C-space>",
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
           scope_incremental = "<nop>",
-node_decremental = "<bs>",
-},
-},
-},
--- @param opts TSConfig
-config = function(_, opts)
-  require("nvim-treesitter.configs").setup(opts)
-end,
-},
-{ "MunifTanjim/nui.nvim", lazy = true },
-{
-"catppuccin/nvim",
-lazy = true,
-name = "catppuccin",
-},
-{ "nvim-tree/nvim-web-devicons", lazy = true },
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    -- @param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+  },
+  { "nvim-tree/nvim-web-devicons" },
   {
     "stevearc/dressing.nvim",
     lazy = true,
@@ -345,7 +344,7 @@ name = "catppuccin",
   },
   {
     "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
+    event = 'VeryLazy',
     opts = {
       options = {
         disabled_filetypes = { 'netrw', 'Trouble' },
