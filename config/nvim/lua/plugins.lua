bc02ba9fc542e6ab273104d6e4aabf92d1bb8758
Brian Riley <97886+brianriley@users.noreply.github.com>
2025-09-01 15:35:25 -0700

    Update mason-lspconfig setup

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -11,21 +11,15 @@ return {
   },
 
   -- lsp
-  {
-    'williamboman/mason-lspconfig.nvim',
-    config = function(_, _)
-      require('mason-lspconfig').setup()
-      require('mason-lspconfig').setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function (server_name) -- default handler (optional)
-          require('lspconfig')[server_name].setup {}
-        end,
-      }
-    end,
-  },
   { 'neovim/nvim-lspconfig' },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+    },
+  },
   {
     "folke/lazydev.nvim",
     ft = "lua", -- only load on lua files
