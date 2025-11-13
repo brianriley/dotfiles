c1d3e558385c0de3a5e531a1942bbb884e12c5e1
Brian Riley <97886+brianriley@users.noreply.github.com>
2025-11-13 08:58:06 -0800

    Some auto styling updates

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,92 +1,92 @@
 local vim = vim
 
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-require('config.lazy')
+require("config.lazy")
 
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
-vim.o.clipboard = 'unnamedplus' -- system clipboard
+vim.o.clipboard = "unnamedplus" -- system clipboard
 vim.o.cursorline = true
-vim.o.expandtab = true          -- convert tabs to spaces
-vim.o.ignorecase = true         -- ignore case in search patterns
-vim.o.number = true             -- show line numbers
-vim.o.scrolloff = 3             -- lines to keep at top and bottom
-vim.o.shiftwidth = 2            -- number of space for each indent
-vim.o.signcolumn = 'yes'        -- add LSP/git signs to the left
-vim.o.sidescrolloff = 3         -- lines to keep at left and right
+vim.o.expandtab = true -- convert tabs to spaces
+vim.o.ignorecase = true -- ignore case in search patterns
+vim.o.number = true -- show line numbers
+vim.o.scrolloff = 3 -- lines to keep at top and bottom
+vim.o.shiftwidth = 2 -- number of space for each indent
+vim.o.signcolumn = "yes" -- add LSP/git signs to the left
+vim.o.sidescrolloff = 3 -- lines to keep at left and right
 vim.o.smartcase = true
 vim.o.smartindent = true
 vim.o.splitbelow = true -- split windows below current
 vim.o.splitright = true -- split windows to the right
 vim.o.tabstop = 2
-vim.o.updatetime = 250  -- speeds up the update for faster completion
+vim.o.updatetime = 250 -- speeds up the update for faster completion
 vim.o.wrap = true
 vim.o.linebreak = true
 
 -- keybindings
 local function nmap(keys, command, opts)
-  opts = opts or {}
-  vim.keymap.set('n', keys, command, opts)
+	opts = opts or {}
+	vim.keymap.set("n", keys, command, opts)
 end
 
-vim.keymap.set('i', '<C-c>', '<Esc>')
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
-nmap('<leader>q', ':q<cr>')
-nmap('<leader>Q', ':qa<cr>')
-nmap('<leader>r', ':r<SPACE>')
-nmap('<leader>v', ':v<SPACE>')
-nmap('<leader>w', ':w<cr>')
-nmap('<leader>W', ':wa<cr>')
-nmap('<leader><leader>', ':b#<cr>')
-nmap('<leader>1', ':!<SPACE>')
-nmap('<leader>,', ':e $MYVIMRC<cr>', { desc = 'Edit vimrc file' })
+nmap("<leader>q", ":q<cr>")
+nmap("<leader>Q", ":qa<cr>")
+nmap("<leader>r", ":r<SPACE>")
+nmap("<leader>v", ":v<SPACE>")
+nmap("<leader>w", ":w<cr>")
+nmap("<leader>W", ":wa<cr>")
+nmap("<leader><leader>", ":b#<cr>")
+nmap("<leader>1", ":!<SPACE>")
+nmap("<leader>,", ":e $MYVIMRC<cr>", { desc = "Edit vimrc file" })
 
 -- Insert blank lines w/o leaving normal mode
-nmap('<leader><CR>', 'o<Esc>')
-nmap('<leader><S-CR>', 'O<Esc>')
+nmap("<leader><CR>", "o<Esc>")
+nmap("<leader><S-CR>", "O<Esc>")
 
 -- move up/down within wrapped lines
-nmap('k', 'gk')
-nmap('<up>', 'gk')
-nmap('j', 'gj')
-nmap('<down>', 'gj')
+nmap("k", "gk")
+nmap("<up>", "gk")
+nmap("j", "gj")
+nmap("<down>", "gj")
 
-nmap('<cr>', ':nohlsearch<cr>', { silent = true }) -- clear highlight on carriage return
+nmap("<cr>", ":nohlsearch<cr>", { silent = true }) -- clear highlight on carriage return
 
 -- find
-nmap('<leader>e', builtin.find_files, { desc = 'Find file' })
-nmap('<leader>b', builtin.buffers, { desc = 'Find buffer' })
-nmap('<leader>E', builtin.git_files, { desc = 'Find in git' })
+nmap("<leader>e", builtin.find_files, { desc = "Find file" })
+nmap("<leader>b", builtin.buffers, { desc = "Find buffer" })
+nmap("<leader>E", builtin.git_files, { desc = "Find in git" })
 
 -- git
-nmap('<leader>gb', ':Git blame<cr>')
+nmap("<leader>gb", ":Git blame<cr>")
 
 -- LSP actions
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    local opts = { remap = false, silent = true, buffer = ev.buf }
-    vim.keymap.set('n', '<leader>li', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', '<leader>ld', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<leader>ln', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>lf', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, opts)
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		local opts = { remap = false, silent = true, buffer = ev.buf }
+		vim.keymap.set("n", "<leader>li", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "<leader>lt", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<leader>lI", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>le", vim.diagnostic.open_float, opts)
+	end,
 })
 
 -- autocomplete with tab
-vim.api.nvim_set_keymap('i', '<Tab>', [[pumvisible() ? "\<C-p>" : "\<Tab>"]], { noremap = true, expr = true })
+vim.api.nvim_set_keymap("i", "<Tab>", [[pumvisible() ? "\<C-p>" : "\<Tab>"]], { noremap = true, expr = true })
 
 -- vim-test
-nmap('<leader>t', ':TestNearest<cr>', { silent = true })
-nmap('<leader>T', ':TestFile<cr>', { silent = true })
+nmap("<leader>t", ":TestNearest<cr>", { silent = true })
+nmap("<leader>T", ":TestFile<cr>", { silent = true })
 
 -- autocommands
-vim.cmd [[
+vim.cmd([[
   augroup jump_to_last_position
     autocmd!
     " Jump to last cursor position in file unless it's invalid or in an event handler
@@ -98,40 +98,40 @@ vim.cmd [[
     autocmd BufReadPost COMMIT_EDITMSG
       \ exe "normal! gg"
   augroup END
-]]
-vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
+]])
+vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
 
 -- LSP diagnostics
 local signs = {
-  Error = " ",
-  Warn = " ",
-  Hint = "󰌵 ",
-  Info = " "
+	Error = " ",
+	Warn = " ",
+	Hint = "󰌵 ",
+	Info = " ",
 }
 
 local signConf = {
-  text = {},
-  texthl = {},
-  numhl = {},
+	text = {},
+	texthl = {},
+	numhl = {},
 }
 
 for type, icon in pairs(signs) do
-  local severityName = string.upper(type)
-  local severity = vim.diagnostic.severity[severityName]
-  local hl = "DiagnosticSign" .. type
-  signConf.text[severity] = icon
-  signConf.texthl[severity] = hl
-  signConf.numhl[severity] = hl
+	local severityName = string.upper(type)
+	local severity = vim.diagnostic.severity[severityName]
+	local hl = "DiagnosticSign" .. type
+	signConf.text[severity] = icon
+	signConf.texthl[severity] = hl
+	signConf.numhl[severity] = hl
 end
 
 vim.diagnostic.config({
-  signs = signConf,
-  virtual_text = true
+	signs = signConf,
+	virtual_text = true,
 })
 
-vim.cmd [[
+vim.cmd([[
   highlight DiagnosticUnderlineError gui=undercurl guisp=Red
   highlight DiagnosticUnderlineWarn gui=undercurl guisp=Orange
   highlight DiagnosticUnderlineInfo gui=undercurl guisp=LightBlue
   highlight DiagnosticUnderlineHint gui=undercurl guisp=LightGrey
-]]
+]])
