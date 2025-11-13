05b74c54f5b54aceb9de6932277b3bcf336320d0
Brian Riley <97886+brianriley@users.noreply.github.com>
2025-11-13 08:59:50 -0800

    Update mini plugins

--- config/nvim/lua/plugins/mini.lua
+++ config/nvim/lua/plugins/mini.lua
@@ -1,46 +1,40 @@
 return {
-  {
-    'nvim-mini/mini.completion',
-    version = false,
-    config = function(_, _)
-      require('mini.completion').setup()
-    end
-  },
-  {
-    'nvim-mini/mini.indentscope',
-    version = false,
-    opts = {
-      symbol = '│',
-    },
-    config = function(_, _)
-      require('mini.indentscope').setup()
-    end
-  },
-  {
-    'nvim-mini/mini.comment',
-    version = false,
-    config = function(_, _)
-      require('mini.comment').setup()
-    end
-  },
-  {
-    'nvim-mini/mini.surround',
-    version = false,
-    opts = {
-      mappings = {
-        delete = 'ds',
-        replace = 'cs',
-      },
-    },
-    config = function(_, _)
-      require('mini.surround').setup()
-    end
-  },
-  {
-    'nvim-mini/mini.statusline',
-    version = false,
-    config = function(_, _)
-      require('mini.statusline').setup()
-    end
-  },
+	{
+		"nvim-mini/mini.completion",
+		version = false,
+		config = function(_, _)
+			require("mini.completion").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.indentscope",
+		version = false,
+		opts = {
+			symbol = "│",
+		},
+		config = function(_, _)
+			require("mini.indentscope").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.comment",
+		version = false,
+		config = function(_, _)
+			require("mini.comment").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.surround",
+		version = false,
+		config = function(_, _)
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		"nvim-mini/mini.statusline",
+		version = false,
+		config = function(_, _)
+			require("mini.statusline").setup()
+		end,
+	},
 }
