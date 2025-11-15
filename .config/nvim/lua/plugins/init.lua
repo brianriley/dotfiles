139746f28340ec5698acd68305028494517a4e0a
Brian Riley <97886+brianriley@users.noreply.github.com>
2025-11-14 19:14:48 -0800

    Cleanup of LSP config

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,185 +2,165 @@ local vim = vim
 local jit = jit
 
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
+	-- lsp
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+				"eslint",
+			},
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
 
-  -- lsp
-  { 'neovim/nvim-lspconfig' },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "mason.nvim" },
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.hover.dictionary,
-          nls.builtins.code_actions.refactoring,
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt,
-        },
-      }
-    end,
-  },
+	-- searching
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-  -- searching
-  {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
+	-- git
+	{ "tpope/vim-fugitive" },
+	{ "tpope/vim-rhubarb" },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function(_, _)
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+					map("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
 
-  -- git
-  { 'tpope/vim-fugitive' },
-  { 'tpope/vim-rhubarb' },
-  {
-    'lewis6991/gitsigns.nvim',
-    config = function(_, _)
-      require('gitsigns').setup({
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
-          map('n', ']c', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function() gs.next_hunk() end)
-            return '<Ignore>'
-          end, { expr = true })
+					map("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+				end,
+			})
+		end,
+	},
 
-          map('n', '[c', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function() gs.prev_hunk() end)
-            return '<Ignore>'
-          end, { expr = true })
-        end
-      })
-    end,
-  },
+	-- tpope
+	{ "tpope/vim-vinegar" },
+	{ "tpope/vim-speeddating" },
 
-  -- tpope
-  { 'tpope/vim-vinegar' },
-  { 'tpope/vim-speeddating' },
+	-- tests
+	{ "vim-test/vim-test" },
 
-  -- tests
-  { 'vim-test/vim-test' },
-
-  -- editor
-  {
-    "nvim-pack/nvim-spectre",
-    config = function(_, _)
-      require('spectre').setup()
-      vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<cr>', {
-        desc = "Toggle Spectre"
-      })
-      vim.keymap.set('n', '<leader>ss', '<cmd>lua require("spectre").open_visual({select_word=true})<cr>', {
-        desc = "Search current word in directory"
-      })
-      vim.keymap.set('n', '<leader>s*', '<cmd>lua require("spectre").open_file_search({select_word=true})<cr>', {
-        desc = "Search on current file"
-      })
-    end
-  },
-  {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-    opts = {},
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics filter.buf=0<cr>",
-        desc = "Buffer Diagnostics (Trouble)"
-      },
-      {
-        "<leader>xX",
-        "<cmd>Trouble diagnostics<cr>",
-        desc = "Diagnostics (Trouble)"
-      },
-      {
-        "<leader>xL",
-        "<cmd>Trouble loclist toggle<cr>",
-        desc = "Location List (Trouble)"
-      },
-      {
-        "<leader>xQ",
-        "<cmd>Trouble qfix toggle<cr>",
-        desc = "Quickfix List (Trouble)"
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    lazy = false,
-    build = ":TSUpdate",
-  },
-  { "MunifTanjim/nui.nvim",        lazy = true },
-  { "nvim-tree/nvim-web-devicons", opts = {} },
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = true,
-    opts = {
-      bigfile = { enabled = true },
-      dashboard = { enabled = true },
-      explorer = { enabled = true },
-      indent = { enabled = true },
-      input = { enabled = true },
-      picker = { enabled = true },
-      notifier = { enabled = true },
-      quickfile = { enabled = true },
-      scope = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
-    },
-  },
-  {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
-    lazy = false,
-  },
+	-- editor
+	{
+		"nvim-pack/nvim-spectre",
+		config = function(_, _)
+			require("spectre").setup()
+			vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<cr>', {
+				desc = "Toggle Spectre",
+			})
+			vim.keymap.set("n", "<leader>ss", '<cmd>lua require("spectre").open_visual({select_word=true})<cr>', {
+				desc = "Search current word in directory",
+			})
+			vim.keymap.set("n", "<leader>s*", '<cmd>lua require("spectre").open_file_search({select_word=true})<cr>', {
+				desc = "Search on current file",
+			})
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		cmd = "Trouble",
+		opts = {},
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qfix toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		lazy = false,
+		build = ":TSUpdate",
+	},
+	{ "MunifTanjim/nui.nvim", lazy = true },
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = true,
+		opts = {
+			bigfile = { enabled = true },
+			dashboard = { enabled = true },
+			explorer = { enabled = true },
+			indent = { enabled = true },
+			input = { enabled = true },
+			picker = { enabled = true },
+			notifier = { enabled = true },
+			quickfile = { enabled = true },
+			scope = { enabled = true },
+			scroll = { enabled = true },
+			statuscolumn = { enabled = true },
+			words = { enabled = true },
+		},
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		dependencies = { { "nvim-tree/nvim-web-devicons", opts = {} } },
+		lazy = false,
+	},
 }
